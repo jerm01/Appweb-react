79adc260cb27b712a109377bfa4d0a6f7bfbc49a
Enrique Alejandro Galván Montes <enrique.agm2177@gmail.com>
2025-07-11 12:09:52 -0600

    fixed things about service worker

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,46 +1,5 @@
-const odsquiz = 'odsquiz-cache-v1';
-const urlsToCache = [
-  '/',
-  '/index.html',
-  '/manifest.json',
-  '/icons/icon-192.png',
-  '/icons/icon-512.png',
-  '/img/odsquiz-hero.png',
-  '/img/ods-info.png',
-  '/img/team-collab.png',
-  '/favicon.ico',
-  '/static/js/bundle.js',
-  '/static/js/main.chunk.js',
-  '/static/js/0.chunk.js',
-  '/static/css/main.css',
-];
+/* eslint-disable no-restricted-globals */
+import { precacheAndRoute } from 'workbox-precaching';
 
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(odsquiz).then(cache => {
-      return cache.addAll(urlsToCache);
-    })
-  );
-});
-
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys => {
-      return Promise.all(
-        keys.map(key => {
-          if (key !== odsquiz) {
-            return caches.delete(key);
-          }
-        })
-      );
-    })
-  );
-});
-
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    }).catch(() => {})
-  );
-});
+// Esta línea será reemplazada automáticamente durante el build por Create React App
+precacheAndRoute(self.__WB_MANIFEST);
